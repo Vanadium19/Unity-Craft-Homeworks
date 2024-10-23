0e1ffb7873255fe112d7c1c0bc97a9cf3bc0f0ff
Vladislav Melnik <GimliVlad@list.ru>
2024-10-23 18:03:31 +0300

    Update parallax

--- Assets/Scripts/Level/LevelBackground.cs
+++ Assets/Scripts/Level/LevelBackground.cs
@@ -1,61 +1,35 @@
-using System;
 using UnityEngine;
 
 namespace ShootEmUp
 {
     public sealed class LevelBackground : MonoBehaviour
     {
-        private float startPositionY;
-        private float endPositionY;
-        private float movingSpeedY;
-        private float positionX;
-        private float positionZ;
+        [SerializeField] private float _startPositionY = 19;
+        [SerializeField] private float _endPositionY = -38;
+        [SerializeField] private float _movingSpeedY = 5;
 
-        private Transform myTransform;
-
-        [SerializeField]
-        private Params m_params;
+        private Transform _transform;
+        private float _positionX;
+        private float _positionZ;
 
         private void Awake()
         {
-            this.startPositionY = this.m_params.m_startPositionY;
-            this.endPositionY = this.m_params.m_endPositionY;
-            this.movingSpeedY = this.m_params.m_movingSpeedY;
-            this.myTransform = this.transform;
-            var position = this.myTransform.position;
-            this.positionX = position.x;
-            this.positionZ = position.z;
+            _transform = transform;
+            _positionX = _transform.position.x;
+            _positionZ = _transform.position.z;
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
-            if (this.myTransform.position.y <= this.endPositionY)
-            {
-                this.myTransform.position = new Vector3(
-                    this.positionX,
-                    this.startPositionY,
-                    this.positionZ
-                );
-            }
-
-            this.myTransform.position -= new Vector3(
-                this.positionX,
-                this.movingSpeedY * Time.fixedDeltaTime,
-                this.positionZ
-            );
+            Move();
         }
 
-        [Serializable]
-        public sealed class Params
+        private void Move()
         {
-            [SerializeField]
-            public float m_startPositionY;
+            if (_transform.position.y <= _endPositionY)
+                _transform.position = new Vector3(_positionX, _startPositionY, _positionZ);
 
-            [SerializeField]
-            public float m_endPositionY;
-
-            [SerializeField]
-            public float m_movingSpeedY;
+            _transform.Translate(Vector3.down * _movingSpeedY * Time.deltaTime);
         }
     }
 }
