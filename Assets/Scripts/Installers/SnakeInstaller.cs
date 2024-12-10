8b7d9bdde84d1492696e8e6e0cefb1d013a82765
Vladislav Melnik <GimliVlad@list.ru>
2024-12-10 15:27:48 +0300

    Add snake installer

---
+++ Assets/Scripts/Installers/SnakeInstaller.cs
@@ -0,0 +1,26 @@
+using Modules;
+using PlayerInput;
+using UnityEngine;
+using Zenject;
+
+namespace Installers
+{
+    public class SnakeInstaller : MonoInstaller
+    {
+        [SerializeField] private Snake _snake;
+        
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<Snake>()
+                .FromInstance(_snake)
+                .AsSingle();
+            
+            Container.BindInterfacesTo<MoveInput>()
+                .AsSingle();
+            
+            Container.BindInterfacesTo<SnakeMoveController>()
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
