239a4b00d82305bf86414c1f4f2344c23581ff23
Vladislav Melnik <GimliVlad@list.ru>
2024-12-10 16:22:23 +0300

    Bind start game

--- Assets/Scripts/Installers/SnakeInstaller.cs
+++ Assets/Scripts/Installers/SnakeInstaller.cs
@@ -1,6 +1,8 @@
+using Core;
 using Modules;
 using PlayerInput;
 using Player;
+using SnakeGame;
 using UnityEngine;
 using Zenject;
 
@@ -8,18 +10,45 @@ namespace Installers
 {
     public class SnakeInstaller : MonoInstaller
     {
+        private const int MaxLevel = 9;
+
         [SerializeField] private Snake _snake;
-        
+        [SerializeField] private Coin _coinPrefab;
+        [SerializeField] private WorldBounds _worldBounds;
+
         public override void InstallBindings()
         {
+            //Snake
             Container.BindInterfacesTo<Snake>()
                 .FromInstance(_snake)
                 .AsSingle();
-            
+
             Container.BindInterfacesTo<MoveInput>()
                 .AsSingle();
-            
+
             Container.BindInterfacesTo<SnakeMoveController>()
+                .AsSingle()
+                .NonLazy();
+
+            //Components
+            Container.BindInterfacesTo<Difficulty>()
+                .AsSingle()
+                .WithArguments(MaxLevel);
+
+            Container.BindInterfacesAndSelfTo<Coin>()
+                .FromInstance(_coinPrefab)
+                .AsCached();
+
+            Container.BindInterfacesTo<WorldBounds>()
+                .FromInstance(_worldBounds)
+                .AsSingle();
+
+            //Core
+            Container.BindInterfacesTo<CoinSpawner>()
+                .AsSingle()
+                .NonLazy();
+
+            Container.BindInterfacesTo<GameManager>()
                 .AsSingle()
                 .NonLazy();
         }
